c6ed754e765c457984297cc1e8d01a606c902fa7
RAJ KUMAR.V <161424347+RAJKUMARV-2005@users.noreply.github.com>
2024-10-04 14:53:45 +0530

    Update button.js

--- button.js
+++ button.js
@@ -1,14 +1,17 @@
 function changeRed(){
     const red=document.getElementById('red');
     red.style.backgroundColor="red";
+    document.body.style.backgroundColor="red";
 }
 function changeBlue(){
     const blue=document.getElementById('blue');
     blue.style.backgroundColor="blue";
+    document.body.style.backgroundColor="blue";
 }
 function changeGreen(){
     const green=document.getElementById('green');
     green.style.backgroundColor="green";
+    document.body.style.backgroundColor="green";
 }
 function reset(){
     const red=document.getElementById('red');
@@ -17,5 +20,6 @@ function reset(){
     blue.style.backgroundColor="";
     const green=document.getElementById('green');
     green.style.backgroundColor="";
+    document.body.style.backgroundColor="";
 
 }
