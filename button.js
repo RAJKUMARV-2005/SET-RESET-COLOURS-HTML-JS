59ecfab1aaffd648de6729476caf9ce7da377422
RAJ KUMAR.V <161424347+RAJKUMARV-2005@users.noreply.github.com>
2024-10-04 14:47:06 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,21 @@
+function changeRed(){
+    const red=document.getElementById('red');
+    red.style.backgroundColor="red";
+}
+function changeBlue(){
+    const blue=document.getElementById('blue');
+    blue.style.backgroundColor="blue";
+}
+function changeGreen(){
+    const green=document.getElementById('green');
+    green.style.backgroundColor="green";
+}
+function reset(){
+    const red=document.getElementById('red');
+    red.style.backgroundColor="";
+    const blue=document.getElementById('blue');
+    blue.style.backgroundColor="";
+    const green=document.getElementById('green');
+    green.style.backgroundColor="";
+
+}
